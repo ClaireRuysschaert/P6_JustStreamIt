8908e5caa537b06e1c58a70e32c25fadf6e81bc0
ClaireRuysschaert <netclaire@hotmail.fr>
2023-11-10 11:58:35 +0100

    feat: Add handleModal function and create infos elements

--- main.js
+++ main.js
@@ -15,24 +15,104 @@ function fetchAndDisplayMovies(url, containerId, isBestMovie = false) {
 
 function displayBestMovie(movie, containerId) {
   const container = document.getElementById(containerId);
-  container.innerHTML = movie.title;
-  container.appendChild(document.createElement("br"));
+  title = document.createElement("h1");
+  title.innerHTML = movie.title;
+  container.appendChild(title);
   const img = document.createElement("img");
   img.src = movie.image_url;
   container.appendChild(img);
   const playButton = document.createElement("button");
   playButton.innerHTML = "Play";
   container.appendChild(playButton);
-  const moreInfosButton = document.createElement("button");
-  moreInfosButton.innerHTML = "More Infos";
-  container.appendChild(moreInfosButton);
   fetch(api_url + movie.id)
     .then((response) => response.json())
     .then((data) => {
       const description = document.createElement("p");
       description.innerHTML = data.description;
       container.appendChild(description);
+
+      var modalContent = document.querySelector("#myModal .modal-content p");
+      
+      var imgElement = document.createElement("img");
+      imgElement.src = movie.image_url;
+      modalContent.appendChild(imgElement);
+
+      var titleElement = document.createElement("h1");
+      titleElement.innerHTML = movie.title;
+      modalContent.appendChild(titleElement);
+
+      var genreElement = document.createElement("p");
+      genreElement.innerHTML = "Genre: " + data.genres;
+      modalContent.appendChild(genreElement);
+
+      var dateElement = document.createElement("p");
+      dateElement.innerHTML = "Date de sortie: " + data.date_published;
+      modalContent.appendChild(dateElement);
+
+      var ratedElement = document.createElement("p");
+      ratedElement.innerHTML = "Rated: " + data.rated;
+      modalContent.appendChild(ratedElement);
+
+      var scoreElement = document.createElement("p");
+      scoreElement.innerHTML = "IMBD Score: " + data.imdb_score;
+      modalContent.appendChild(scoreElement);
+
+      var directorsElement = document.createElement("p");
+      directorsElement.innerHTML = "Réalisateur: " + data.directors;
+      modalContent.appendChild(directorsElement);
+
+      var actorsElement = document.createElement("p");
+      actorsElement.innerHTML = "Acteurs: " + data.actors;
+      modalContent.appendChild(actorsElement);
+
+      var durationElement = document.createElement("p");
+      durationElement.innerHTML = "Durée: " + data.duration + " minutes";
+      modalContent.appendChild(durationElement);
+
+      var countriesElement = document.createElement("p");
+      countriesElement.innerHTML = "Pays: " + data.countries;
+      modalContent.appendChild(countriesElement);
+
+      var boxOfficeElement = document.createElement("p");
+      boxOfficeElement.innerHTML = "Box Office: " + data.worldwide_gross_income;
+      modalContent.appendChild(boxOfficeElement);
+
+      var resumeElement = document.createElement("p");
+      resumeElement.innerHTML = "Résumé: " + data.long_description;
+      modalContent.appendChild(resumeElement);
     });
+
+
+    
+  handleModal("myModal", "moreInfosButton", "close");
+}
+
+function handleModal(modalId, triggerId, closeClass) {
+  // Get the modal
+  var modal = document.getElementById(modalId);
+
+  // Get the button that opens the modal
+  var trigger = document.getElementById(triggerId);
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName(closeClass)[0];
+
+  // When the user clicks the button, open the modal 
+  trigger.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
 
 function displayMovies(movies, containerId) {
@@ -64,3 +144,4 @@ const moviesGenres = ["Biography", "Comedy", "Sci-Fi"];
 moviesGenres.forEach((genre) => {
   fetchAndDisplayMovies(api_url + sort_by_imdb_score + `&genre=${genre}&page_size=7`, `${genre}Movies`);
 });
+
